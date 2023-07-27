f68621b68308492d754810ee930cdff9e4aa1084
Olatunji Martins <martinsolatunji2@gmail.com>
2023-07-27 11:41:36 +0100

    update: added objectives section.

---
+++ components/card/Objective.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="bg-[#F3F3F3] rounded-[1.875rem] p-5 my-3">
+      <div class="flex justify-between  items-center m-5">
+        <span class="text-[1.5rem] font-semibold">{{ data.title }}</span>
+        <SvgArrow class="w-[18%] sm:w-auto" />
+      </div>
+  
+      <div class="flex justify-center">
+        <img :src="data.image" class="w-[50%]" />
+      </div>
+  
+      <div class="m-5">
+        <p class="text-[1rem] line-clamp-3 md:line-clamp-none">
+          {{ data.body }}
+        </p>
+      </div>
+    </div>
+  </template>
+
+  <script setup>
+  defineProps ({
+    data: {
+      type: Object,
+      default: () => ({})
+    }
+  })
+  </script>
+
+  
---
+++ components/home/Objectives.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="my-container">
+        <div 
+          v-motion
+          :visible="scaleInVariants.visibleOnce"
+          :initial="scaleInVariants.initial"
+          class="lg:flex justify-between gap-6 my-8 sm:my-32">
+            <CardObjective v-for="card in cards" :data="card" />
+        </div>
+    </div>
+</template>
+
+<script setup>
+import missionImage from '@/assets/images/MissionImage.png'
+import visionImage from '@/assets/images/VisionImage.png'
+
+// data for objectives cards.
+const cards = reactive([
+  {
+    image: missionImage,
+    title: 'Our Mission',
+    body: 'our mission is to revolutionize the financial landscape by empowering individuals and businesses with innovative and secure financial solutions. We strive to bridge the gap between traditional banking systems and cutting-edge technology, ensuring a seamless and inclusive financial experience ....',
+  },
+  {
+    image: visionImage,
+    title: 'Our Vision',
+    body: 'Our vision is to create a world where financial freedom is accessible to everyone, irrespective of their background or location. We envision a future where financial decisions are driven by data-driven insights, smart automation, and transparent practices, empowering individuals to make informed cho ....',  
+  }
+])
+</script>
