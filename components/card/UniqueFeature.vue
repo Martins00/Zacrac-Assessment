d0749b03514a3de3ee752182a0e5e6beba4d9fee
Olatunji Martins <martinsolatunji2@gmail.com>
2023-07-27 11:43:54 +0100

    update: added unique features section.

---
+++ components/card/UniqueFeature.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="mr-4 mb-4">
+    <component :is="data.icon" class="bg-[#96EF7644] w-[47px] h-[47px] p-2 rounded-[10px]" />
+    <h3 class="text-[1.125rem] font-semibold my-2">{{ data.title }}</h3>
+    <p class="text-[0.8750rem]">{{ data.body }}</p>
+  </div>
+</template>
+
+<script setup>
+defineProps ({
+  data: Object
+})
+</script>
+
